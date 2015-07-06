591516da6688fc3d496c0117006d031709837f26
Kjell Magne Fauske <kjellmf@gmail.com>
2015-07-06 14:33:26 +0200

    Remove bootstrap static files
    First step of material design refactoring

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
-angular.module('symbolApp', ['ngAnimate', 'ui.router', 'ngClipboard', 'mgcrea.ngStrap', 'ngSanitize'])
+angular.module('symbolApp', ['ngMaterial', 'ui.router', 'ngClipboard'])
     .constant("config", {
         BLANK_PATH: "assets/img/blank.png",
         SVG_PATH: "svg/MIL_STD_2525D_Symbols/"
